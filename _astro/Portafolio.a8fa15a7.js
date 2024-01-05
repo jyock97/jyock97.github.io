4dfbdd19f8367e1e767525e9f90de095f67ec601
Github Action <github@actions.com>
2024-01-05 17:07:35 +0000

    Build 7424618722

---
+++ _astro/Portafolio.a8fa15a7.js
@@ -0,0 +1,1 @@
+import{P as i}from"./Portafolio.166a9926.js";import"./index.ed373d49.js";import"./Header.40b5c0ae.js";import"./index.5d28df72.a45f9886.js";export{i as default};
---
+++ _astro/profession.0b2b83b1.js
@@ -0,0 +1,51 @@
+const e="profession.md",n="profession",t="profession",o="",i={professions:[{title:"Backcountry",subtitle:"2018 - present",imageSrc:"/Backcountry.png",experience:[{title:"On-Call Rotation",descriptions:["24/7 disponibility for a week every month.","When in shift, be prepared to handle and remediate any error that could happen for any of the critical systems of the company."]},{title:"Lead Software Engineer",descriptions:["In charge of a Team, managing their personal growth","Code review and quality assurance, making sure all changes to the code follow the Company architectural standards.","Planification and Design of new features requested by stakeholders."]},{title:"Continuous Delivery / Continuous Integration",descriptions:["Create and mantain deployment pipelines for the different systems (Frontend and Backend) that are used by the main Page"]},{title:"UI Improvements",descriptions:["Work with the UI and UX teams to improve and add new features to the Page"]},{title:"New Product Review's System",descriptions:["Help coordinate the deprecation of an old review's system and the integration of a new one."]},{title:"New Chat Integration",descriptions:["Integration of a new Chat UI in the site.","Help with the integration with a third-party communication service used by the company's Customer service.","Be part of the design and implementation of a secure payment widget used by Customers to securely submit orders"]}]}]},a={type:"content",filePath:"/home/runner/work/jyock97.github.io.source/jyock97.github.io.source/src/content/profession/profession.md",rawData:`
+professions: [
+    {
+        title: "Backcountry",
+        subtitle: "2018 - present",
+        imageSrc: '/Backcountry.png',
+        experience: [
+            {
+                title: "On-Call Rotation",
+                descriptions: [
+                    "24/7 disponibility for a week every month.",
+                    "When in shift, be prepared to handle and remediate any error that could happen for any of the critical systems of the company."
+                ]
+            },
+            {
+                title: "Lead Software Engineer",
+                descriptions: [
+                    "In charge of a Team, managing their personal growth",
+                    "Code review and quality assurance, making sure all changes to the code follow the Company architectural standards.",
+                    "Planification and Design of new features requested by stakeholders."
+                ]
+            },
+            {
+                title: "Continuous Delivery / Continuous Integration",
+                descriptions: [
+                    "Create and mantain deployment pipelines for the different systems (Frontend and Backend) that are used by the main Page"
+                ]
+            },
+            {
+                title: "UI Improvements",
+                descriptions: [
+                    "Work with the UI and UX teams to improve and add new features to the Page"
+                ]
+            },
+            {
+                title: "New Product Review's System",
+                descriptions: [
+                    "Help coordinate the deprecation of an old review's system and the integration of a new one."
+                ]
+            },
+            {
+                title: "New Chat Integration",
+                descriptions: [
+                    "Integration of a new Chat UI in the site.",
+                    "Help with the integration with a third-party communication service used by the company's Customer service.",
+                    "Be part of the design and implementation of a secure payment widget used by Customers to securely submit orders"
+                ]
+            }
+        ]
+    }
+]`};export{a as _internal,o as body,n as collection,i as data,e as id,t as slug};
